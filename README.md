933fe6f264743482d9cccde43cd092f56d642735
Adavid <Adavidguevara@protonmail.com>
2023-04-28 15:28:12 -0500

    Fourth Commit

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 - Se incluye el uso de items.
 - Se guarda la informacion en una base datos (MySQL).
+- Este programa fue desarrollado en Linux.
 
-* Este programa fue desarrollado en Linux.
+#### Nota: En este programa se intento usar un ciclo while persistiendo en el uso de Items, se puede notar que tambien nos hace falta un item. Esto ultimo parece indicar que esto se relaciona al uso de items (se investigara mas el tema) y no al uso de la funcion de javascript para hacer el scroll de manera "directa" usada en la version=1,1.
--- quotesToScrapy/quotesToScrapy/spiders/scroll.py
+++ quotesToScrapy/quotesToScrapy/spiders/scroll.py
@@ -1,7 +1,8 @@
-import scrapy
+from scrapy_playwright.page import PageMethod
+from scrapy.selector import Selector
 from scrapy.loader import ItemLoader
 from ..items import Quotes
-from scrapy_playwright.page import PageMethod
+import scrapy
 
 
 class ScrollSpider(scrapy.Spider):
@@ -13,22 +14,30 @@ class ScrollSpider(scrapy.Spider):
             callback=self.parse,
             meta={
                 "playwright": True,
+                "playwright_page_methods": [PageMethod("wait_for_selector", ".quote")],
                 "playwright_include_page": True,
-                "playwright_page_methods": [
-                    PageMethod("wait_for_selector", "div.quote"),
-                    PageMethod(
-                        "evaluate",
-                        "setInterval(function () {var scrollingElement = (document.scrollingElement || document.body);scrollingElement.scrollTop = scrollingElement.scrollHeight;}, 200);",
-                    ),
-                    PageMethod("wait_for_load_state", "networkidle"),
-                ],
             },
+            errback=self.close_page,
         )
 
-    def parse(self, response):
-        for quote in response.xpath("//div[@class='quote']"):
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
+        # Se indica de manera explicita el numero de items:
+        contador = 2
+        while True:
+            await page.evaluate("window.scrollBy(0, document.body.scrollHeight)")
+            await page.wait_for_selector(f".quote:nth-child({contador * 10})")
+            if contador == 10:
+                break
+            contador += 1
+        s = Selector(text=await page.content())
+        await page.close()
+        for quote in s.xpath("//div[@class='quote']"):
             item = ItemLoader(Quotes(), quote)
             item.add_xpath("phrase", ".//span[@class='text']/text()")
             item.add_xpath("author", ".//span[2]/small/text()")
             yield item.load_item()
-        
+
+    async def close_page(self, failure):
+        page = failure.request.meta["playwright_page"]
+        await page.close()
